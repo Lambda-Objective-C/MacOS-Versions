09590c23dfc2ee91ca75e25aafc8f871f535465b
MasterYourMoney <seschwan@yahoo.com>
2019-10-29 15:59:04 -0600

    Project Setup

---
+++ MacOS-Versions-Obj-C/MacOS-Versions-Obj-C/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacOS-Versions-Obj-C
+//
+//  Created by Seschwan on 10/29/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MacOS-Versions-Obj-C/MacOS-Versions-Obj-C/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MacOS-Versions-Obj-C
+//
+//  Created by Seschwan on 10/29/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
